8486c10032bf589cc09250a5f70caf3c132db791
sandicomm <117803353+sandicomm@users.noreply.github.com>
2023-12-05 23:34:08 +0700

    Update blog.js

--- js/blog.js
+++ js/blog.js
@@ -4,9 +4,10 @@ const side = document.querySelectorAll(".article-sidebar h2")
 
 for(let i=0; i<side.length;i++){
    side[i].addEventListener("click", ()=>{
+      let rand = Math.floor(Math.random() * (14-1) +1)
       let title = main.textContent
       main.textContent = side[i].textContent
       side[i].textContent = title
-      img.src = `./assets/foto/foto${i+1}.jpg`
+      img.src = `./assets/foto/foto${rand}.jpg`
    })
 }
